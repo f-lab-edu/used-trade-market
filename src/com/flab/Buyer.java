478c59974e3dddae2eccf6043b314e946a8d33d3
YoungHyun Bae <byh2949@gmail.com>
2022-02-08 23:27:54 +0900

    첫 피드백 이후 수정 내용 커밋
    리뷰 내용 확인하고 javadoc 문서를 위해 주석 정리했고, 메소드 return type, 파라미터 수정

--- src/com/flab/Buyer.java
+++ src/com/flab/Buyer.java
@@ -3,84 +3,151 @@ package com.flab;
 import java.util.Date;
 
 /**
- * 구매자
+ * 중고거래 구매자 상태 및 행위를 나타내는 클래스
+ *
+ * @author 배영현
+ * @version 1.0
+ *
  * */
 public class Buyer {
-    //  택배 배송지
+    /** 
+     * 택배 배송지
+     * */
     private String shippingAddress;
-    // 택배 배송시 요청사항 번호
+
+    /** 
+     * 택배 배송시 요청사항 번호
+     * */
     private int requestmentNo;
-    // 번개포인트
+    
+    /** 
+     * 번개포인트
+     * */
     private int point;
-    //  결제금액
+    
+    /** 
+     * 결제금액
+     * */ 
     private int payMoney;
-    // 회사(중개자)
+    
+    /** 
+     * 회사(중개자)
+     * */
     private Company company;
-    // 구매일자
+    
+    /** 
+     * 구매일자
+     * */
     private Date buydate;
 
-    // 찜하기
-    protected void zzim(String itemNo) {
-
+    /**
+     * 원하는 물건을 찜하는 메소드
+     * @param itemNo 상품 번호
+     * @return 찜이 정상적으로 됐는지 안됐는지 boolean 타입
+     *
+     * */
+    protected boolean zzim(String itemNo) {
+        boolean result = false;
+        return result;
     }
 
-    // 채팅방 오픈
+    /**
+     * 구매예정자와 판매자가 채팅할 수 있도록 채팅방을 오픈하는 메소드
+     * @param itemNo 상품 번호
+     * */
     protected void sendMessage(String itemNo) {
 
     }
 
-    // 택배배송
-    protected void courierServiceBuy(String itemNo) {
-        sendMoneyBuyerToCompany();
-        delivery();
+    /**
+     * 구매자가 직거래가 아닌 택배 배송을 선택했을 때 사용하는 메소드
+     * @param itemNo 상품번호
+     * return result  구매가 정상적으로 일어났는지 boolean 타입으로 확인
+     * */
+    protected boolean courierServiceBuy(String itemNo) {
+        boolean result = false;
+
+        sendMoneyBuyerToCompany(itemNo);
+        delivery(itemNo);
 
         if(this.purchaseConfirmation(itemNo)) {
-               company.sendMoneyCompanyToSeller();
-               addPoint();
+            // 들여쓰기 수정
+            company.sendMoneyCompanyToSeller(itemNo);
+            addPoint(itemNo);
         } else {
-            company.sendMoneyCompanyToBuyer();
+            company.sendMoneyCompanyToBuyer(itemNo);
         }
+        return result;
     }
 
-    // 직거래
-    protected void directTransaction(String itemNo) {
-        sendMoneyBuyerToCompany();
+    /**
+     * 구매자가 직거래를 선택했을 때 사용하는 메소드
+     * @param itemNo 상품번호
+     * return result  구매가 정상적으로 일어났는지 boolean 타입으로 확인
+     * */
+    protected boolean directTransaction(String itemNo) {
+        boolean result = false;
+        sendMoneyBuyerToCompany(itemNo);
 
         if(this.purchaseConfirmation(itemNo)) {
-            company.sendMoneyCompanyToSeller();
-            addPoint();
+            company.sendMoneyCompanyToSeller(itemNo);
+            addPoint(itemNo);
         } else {
-            company.sendMoneyCompanyToBuyer();
+            company.sendMoneyCompanyToBuyer(itemNo);
         }
+        return result;
     }
 
-    // 구매확정
+    /**
+     * 구매예정자가 상품 구매 후 구매를 확정하는지 확인하는 메소드
+     * @param itemNo 상품번호
+     * @return result 구매확정 Y/N
+     * */
     protected boolean purchaseConfirmation(String itemNo) {
         boolean result = false;
-        if(autopurchaseConfirmation()) {
+        if(autopurchaseConfirmation(itemNo)) {
             result = true;
         }
         return result;
     }
 
-    // 구매자 -> 중개자에게 비용 지급
-    protected void sendMoneyBuyerToCompany() {
+    /**
+     * 구매자 -> 중계자에게 비용 지급하는 메소드
+     * @param itemNo 상품번호
+     * @return result 비용 정상 지급 Y/N
+     * */
+    protected boolean sendMoneyBuyerToCompany(String itemNo) {
+        boolean result = false;
+        return result;
     }
 
-    // 포인트 지급
-    protected void addPoint() {
+    /**
+     * 구매자에게 포인트 지급
+     * @param itemNo 상품번호
+     * @return 포인트 정상 지급 Y/N
+     * */
+    protected boolean addPoint(String itemNo) {
+        boolean result = false;
+        return result;
     }
 
-    // 배송
-    protected void delivery() {
+    /**
+     * 상품 배송 메소드
+     * @param itemNo 상품번호
+     * @return result 정상배송 Y/N
+     * */
+    protected boolean delivery(String itemNo) {
+        boolean result = false;
+        return result;
     }
 
-    // 구매 후 3일 지나면 자동 구매확정 체크
-    protected boolean autopurchaseConfirmation() {
+    /**
+     * 구매자가 구매 확정버튼을 3일간 누르지 않으면 자동 구매 확정
+     * @param itemNo 상품번호
+     * @return result  자동 구매확정 Y/N
+     * */
+    protected boolean autopurchaseConfirmation(String itemNo) {
         boolean result=  true;
-
         return true;
     }
-
-
 }
--- src/com/flab/Item.java
+++ src/com/flab/Item.java
@@ -1,31 +1,74 @@
 package com.flab;
-
+/**
+ * 상품 속성 클래스
+ *
+ * @author 배영현
+ * @version 1.0
+ * */
 public class Item {
-    // 상품번호
+    /**
+     * 상품번호
+     * */
     private String itemNo;
-    // 상품 이미지
+
+    /**
+     * 상품이미지
+     * */
     private String imgNo;
-    // 제목
+
+    /**
+     * 게시글 제목
+     * */
     private String title;
-    // 대분류
+
+    /**
+     * 대분류 카테고리 번호
+     * */
     private int bigCategoryNo;
-    // 중분류
+
+    /**
+     * 중분류 카테고리 번호
+     * */
     private int middleCategoryNo;
-    // 소분류
+
+    /**
+     * 소분류 카테고리 번호
+     * */
     private int smallCategoryNo;
-    // 거래지역
+
+    /**
+     * 거래지역
+     * */
     private String address;
-    // 상태(중고, 새상품)
+
+    /**
+     * 중고상품 Y/N
+     * */
     private Boolean UsedYN;
-    // 교환 타입(교환불가, 교환가능)
+
+    /**
+     * 교환 타입(교환불가, 교환가능)
+     * */
     private Boolean exchangeYN;
-    // 가격
+
+    /**
+     * 가격
+     * */
     private int price;
-    // 설명
+
+    /**
+     * 설명
+     * */
     private String remark;
-    // 태그
+
+    /**
+     * 태그
+     * */
     private String tag;
-    // 수량
+
+    /**
+     * 수량
+     * */
     private int itemNum;
 
 }
