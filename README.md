16e81e601cb09b5e77919589c408505bedbb200f
YoungHyun Bae <byh2949@gmail.com>
2022-02-21 21:59:21 +0900

    README추가, 사용자 클래스 로그 적용
    readme에 프로젝트 진행 순서 정리, Member 클래스 javadoc 수정 및 logger 적용

---
+++ README.md
@@ -0,0 +1,5 @@
+# 중고 거래 시스템
+
+1. 프로토타입 작성
+2. 로그 적용 => 블로그 정리
+   https://fightingdeveloper.tistory.com/7 
--- src/com/flab/Member.java
+++ src/com/flab/Member.java
@@ -1,4 +1,11 @@
 package com.flab;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 회원 일반 기능 정의 클래스
  *
@@ -6,6 +13,9 @@ package com.flab;
  * @version 1.0
  * */
 public class Member {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     /**
      * 회원 번호
      * */
@@ -28,32 +38,72 @@ public class Member {
 
     /**
      * 비로그인 시 로그인 화면으로 보내는 메소드
-     * @param member 멤버 정보
      * */
-    public void goLogin(Member member) {
+    public void goLogin() {
     }
 
     /**
-     * 회원가입 메소드
+     * 회원 정보인 <code>member</code>를 통해 회원이 서비스를 이용할 수 있도록 가입을 할 수 있도록 한다.
+     * <code>member</code>가 <code>null</code>인 경우에는 NullPointException을 반환한다.
+     * 정상적으로 회원 가입이 된 경우 <code>true</code>를 반환한다.
      * @param member 회원 정보 입력
      * @return result 회원 가입 정상 Y/N
      * */
     public boolean join(Member member) {
         boolean result = false;
+
+        logger.info("-------------------------");
+        logger.info("Member join method");
+        logger.info("parameter : " + member);
+        logger.info("-------------------------");
+
         return result;
     }
 
     /**
-     * 회원 추천 상품 목록 보기
+     * 회원 정보가 <code>member</code>인 사용자를 통해 추천 상품 리스트를 나열해 보여준다.
+     * <code>member</code>가 <code>null</code>인 경우에는 NullPointException을 반환한다.
      * @param member 회원 정보
      * */
-    public void recommendList(Member member) {
+    public List<Item> recommendList(Member member) {
+
+        logger.info("-------------------------");
+        logger.info("Item recommendList");
+        logger.info("parameter member : " + member);
+        logger.info("return List");
+        logger.info("-------------------------");
+
+        return new ArrayList<Item>();
     }
 
     /**
-     * 상품 목록 보는 메소드
+     * 상품 목록을 보여주는 메서드로 <code>item</code>을 받아 item의 카테고리 정보를 통해 상품을 보여준다.
+     * ArrayList로 상품 정보를 담아 반환한다.
+     * <code>item</code>이 <code>null</code>인 경우에는 NullPointException을 반환한다.
      * @param item 상품 카테고리 체크를 위해
+     * @return ArrayList<Item>
      * */
-    public void viewItemList(Item item) {
+    public List<Item> viewItemList(Item item) {
+        logger.info("-------------------------");
+        logger.info("Item viewItemList");
+        logger.info("parameter Item(category) : " + item);
+        logger.info("return List");
+        logger.info("-------------------------");
+        return new ArrayList<Item>();
+    }
+
+    /**
+     * 회원 정보가 <code>member</code>인 객체를 확인하고 정보가 없으면 false를 반환하고 정보가 있으면 true를 반환한다.
+     * 회원만 사용할 수 있도록 체크하는 메서드
+     * @param member
+     * @return result
+     * */
+    public boolean isRegistered(Member member) {
+        boolean result = true;
+        logger.info("-------------------------");
+        logger.info("isRegistered Method");
+        logger.info("parameter : " + member);
+        logger.info("-------------------------");
+        return result;
     }
 }
