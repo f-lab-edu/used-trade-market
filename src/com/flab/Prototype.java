88bf90c8cf3585bbf7c4640e43f1196445c607d7
YoungHyun Bae <byh2949@gmail.com>
2022-02-21 22:42:31 +0900

    메인 클래스 피드백 내용 수정
    1. 회원만 이용가능하도록 회원인지 체크하는 메소드 추가
    2. 직거래인지 택배배송인지 확인하는 메소드 이름 변경(checkDeal -> isDirectTransaction)
    3. if문 제거 -> 삼항연산자로 변경
    4. 회원가입 메소드 member.join() 파라미터로 member  객체 추가

--- src/com/flab/Prototype.java
+++ src/com/flab/Prototype.java
@@ -1,9 +1,12 @@
 package com.flab;
 
-import java.lang.reflect.Member;
+import org.apache.log4j.BasicConfigurator;
+
+import java.util.List;
 
 public class Prototype {
     public static void main(String[] args) {
+        BasicConfigurator.configure();
 
         /** 
          * 임시 객체
@@ -20,17 +23,15 @@ public class Prototype {
         member.join(member);
 
         //추천 리스트 보여주기
-        List<Item> recommendList = member.recommendList();
+        List<Item> recommendList = member.recommendList(member);
 
         // 상품 리스트 보여주기
-        List<Item> normalList = member.viewItemList();
+        List<Item> normalList = member.viewItemList(item);
 
-        // 회원만 이용 가능
-        //if(member.getId() == null || member.getId().isEmpty()) {
         /**
          * 회원인지 아닌지 체크 후 회원이 아니면 로그인 메소드를 타도록 함.
          * */
-        if(!member.isRegistered()) {
+        if(!member.isRegistered(member)) {
             member.goLogin();
         }
 
@@ -44,19 +45,24 @@ public class Prototype {
          * 구매자
          * */
         // 상품 선택
-        buyer.selectItem(item.itemNo);
+        Item selectItem = buyer.selectItem(item.itemNo);
 
         // 찜하기
-        buyer.zzim(item.itemNo);
+        buyer.zzim(selectItem.itemNo);
 
         // 연락하기
-        buyer.sendMessage(item.itemNo);
+        buyer.sendMessage(selectItem.itemNo);
+
+        /**
+         * 구매자가 거래방법 체크하는 메소드 추가
+         * */
+        String selectDealType = buyer.selectDealType("dealType");
 
         /**
          * isDirectTransaction()을 통해 택배배송인지 직거래인지 체크해서 true 직거래 그렇지 않으면 택배거래를 하도록 함
          * */
         // 삼항연산자로 변경
-        buyer.isDirectTransaction(item.itemNo) ? buyer.directTransaction(item.itemNo) : buyer.courierServiceBuy(item.itemNo);
+        boolean check = buyer.isDirectTransaction(selectDealType) ? buyer.directTransaction(selectItem.itemNo) : buyer.courierServiceBuy(selectItem.itemNo);
 
     }
 }
