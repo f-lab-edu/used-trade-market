bceedb9946e5bf5ba00d97d535bdbd73888ddd46
YoungHyun Bae <byh2949@gmail.com>
2022-03-14 21:35:35 +0900

    메서드 위치 변경된 클래스
    메서드가 다른 클래스로 옮겨져 삭제해도 된다고 판단되는 클래스

--- app/src/main/java/com/flab/Buyer.java
+++ app/src/main/java/com/flab/Buyer.java
@@ -42,142 +42,16 @@ public class Buyer {
     private Date buydate;
 
     /**
-     * 상품 번호가 <code>itemNo</code>인 상품을 찜 리스트에 추가한다.
-     * 정상적으로 찜리스트에 추가된 경우에는 <code>true</code>를 반환한다. 이미 찜리스트에 있는 경우에는 중복되어 추가되지 않고 <code>true</code>를 반환한다.
-     * <code>itemNo</code>가 <code>null</code>인 경우에는 NullPointException을 반환한다.
-     * @param itemNo 상품 번호
-     * @return 찜이 정상적으로 됐는지 안됐는지 boolean 타입
-     *
-     * */
-    protected boolean zzim(Long itemNo) {
-        boolean result = false;
-        logger.info("----------------");
-        logger.info("zzim method");
-        logger.debug("parameter itemNo : " + itemNo);
-        logger.info("----------------");
-        return result;
-    }
-
-    /**
      * 상품 번호가 <code>itemNo</code>인 상품의 상품 등록자 아이디를 통해 구매예정자와 판매자가 채팅할 수 있도록 채팅방을 오픈하는 메소드
      * <code>itemNo</code>가 <code>null</code>인 경우에는 NullPointException을 반환한다.
      * @param itemNo 상품 번호
      * */
     protected void sendMessage(Long itemNo) {
+        System.out.println("연락하기 채팅방 미구현");
         logger.info("-------------------");
         logger.info("sendMessage method");
         logger.debug("parameter itemNo : " + itemNo);
         logger.info("-------------------");
-    }
-
-    /**
-     * 구매자가 상품 번호가 <code>itemNo</code>인 상품의 거래방법을 택배 배송을 선택했을 때 사용하는 메소드
-     * <code>itemNo</code>가 <code>null</code>인 경우에는 NullPointException을 반환한다.
-     * @param itemNo 상품번호
-     * return result  구매가 정상적으로 일어났는지 boolean 타입으로 확인
-     * */
-    protected boolean courierServiceBuy(Long itemNo) {
-        boolean result = false;
-        logger.info("----------------------");
-
-        Company company = new Company();
-        sendMoneyBuyerToCompany(itemNo);
-        delivery(itemNo);
-
-        if(this.purchaseConfirmation(itemNo)) {
-            company.sendMoneyCompanyToSeller(itemNo);
-            addPoint(itemNo);
-        } else {
-            company.sendMoneyCompanyToBuyer(itemNo);
-        }
-        logger.info("----------------------");
-        return result;
-
-    }
-
-    /**
-     * 구매자가 상품 번호가 <code>itemNo</code>인 상품의 거래방법을 직거래를 선택했을 때 사용하는 메소드로 거래방법
-     * <code>itemNo</code>가 <code>null</code>인 경우에는 NullPointException을 반환한다.
-     * @param itemNo 상품번호
-     * return result  구매가 정상적으로 일어났는지 boolean 타입으로 확인
-     * */
-    protected boolean directTransaction(Long itemNo) {
-        boolean result = false;
-        logger.info("----------------------");
-        logger.info("directTransaction method");
-        logger.debug("parameter itemNo : " + itemNo);
-        sendMoneyBuyerToCompany(itemNo);
-
-        Company company = new Company();
-
-        if(this.purchaseConfirmation(itemNo)) {
-            company.sendMoneyCompanyToSeller(itemNo);
-            addPoint(itemNo);
-        } else {
-            company.sendMoneyCompanyToBuyer(itemNo);
-        }
-        logger.info("----------------------");
-        return result;
-    }
-
-    /**
-     * 상품 번호가 <code>itemNo</code>인 상품에 대해 구매자가 상품 구매 후 문제가 없다면 구매를 확정하는지 확인하는 메소드로
-     * 구매 확정 상태가 변경된다.
-     * <code>itemNo</code>가 <code>null</code>인 경우에는 NullPointException을 반환한다.
-     * @param itemNo 상품번호
-     * @return result 구매확정 Y/N
-     * */
-    protected boolean purchaseConfirmation(Long itemNo) {
-        boolean result = false;
-        logger.info("----------------------");
-        logger.info("purchaseConfirmation method");
-        logger.debug("parameter itemNo : " + itemNo);
-        if(autopurchaseConfirmation(itemNo)) {
-            result = true;
-        }
-        logger.info("----------------------");
-        return result;
-    }
-
-    /**
-     * 상품 번호가 <code>itemNo</code>인 상품을 구매자가 구입 후 구매 확정이 되기 전 구매자가 중계자에게 비용을 맡기는 기능을 하는 메소드
-     * <code>itemNo</code>가 <code>null</code>인 경우에는 NullPointException을 반환한다.
-     * @param itemNo 상품번호
-     * @return result 비용 정상 지급 Y/N
-     * */
-    protected boolean sendMoneyBuyerToCompany(Long itemNo) {
-        boolean result = false;
-        logger.info("----------------------");
-        logger.info("sendMoneyBuyerToCompany method");
-        logger.debug("parameter itemNo : " + itemNo);
-        logger.info("----------------------");
-        return result;
-    }
-
-    /**
-     * 상품 번호가 <code>itemNo</code>인 상품에 대한 포인트를 구매자에게 지급하는 메소드
-     * <code>itemNo</code>가 <code>null</code>인 경우에는 NullPointException을 반환한다.
-     * @param itemNo 상품번호
-     * @return 포인트 정상 지급 Y/N
-     * */
-    protected boolean addPoint(Long itemNo) {
-        boolean result = false;
-        logger.info("----------------------");
-        logger.info("addPoint Method");
-        logger.debug("parameter itemNo : " + itemNo);
-        logger.info("----------------------");
-        return result;
-    }
-
-    /**
-     * 상품 배송 메소드
-     * <code>itemNo</code>가 <code>null</code>인 경우에는 NullPointException을 반환한다.
-     * @param itemNo 상품번호
-     * @return result 정상배송 Y/N
-     * */
-    protected boolean delivery(Long itemNo) {
-        boolean result = false;
-        return result;
     }
 
     /**
@@ -196,19 +70,6 @@ public class Buyer {
     }
 
     /**
-     * 사용자가 선택한 상품의 정보를 갖고 오는 메소드로 <code>itemNo</code>인 상품 번호를 파라미터로 받아 상품 정보를 갖고오는 메소드
-     * <code>itemNo</code>가 <code>null</code>인 경우에는 NullPointException을 반환한다.
-     * @param itemNo 상품번호
-     * */
-    protected Item selectItem(Long itemNo) {
-        logger.info("-------------------");
-        logger.info("selectItem method");
-        logger.debug("parameter itemNo : " + itemNo);
-        logger.info("-------------------");
-        return new Item();
-    }
-
-    /**
      * selectDealType 구매자가 거래 방법을 선택하는 메소드로 <code>dealType</code>을 통해 직거래인지 택배배송인지 체크하여 반환한다.
      * <code>dealType</code>이 <code>null</code>인 경우에는 NullPointException을 반환한다.
      * */
@@ -218,21 +79,5 @@ public class Buyer {
         logger.debug("parameter dealType : " + dealType);
         logger.info("----------------------");
         return dealType;
-    }
-
-    /**
-     * <code>dealType</code>는 거래 방법을 의미하는 파라미터로 해당 파라미터를 통해 직거래이면 <code>true</code> 직거래가 아니라 택배거래이면
-     * <code>false</code>를 반환한다.
-     * <code>dealType</code>이 없으면 NullPointException을 반환한다.
-     * @param selectDealTypeCode
-     * return result
-     * */
-    protected boolean isDirectTransaction(int selectDealTypeCode) {
-        boolean result = false;
-        logger.info("----------------------");
-        logger.info("isDirectTransaction method");
-        logger.debug("parameter dealType : " + selectDealTypeCode);
-        logger.info("----------------------");
-        return result;
     }
 }
--- app/src/main/java/com/flab/Company.java
+++ app/src/main/java/com/flab/Company.java
@@ -13,32 +13,5 @@ public class Company {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    /**
-     * 구매확정 후 판매자에게 돈 지불하는 메소드로 <code>itemNo</code>인 상품을 찾아 금액을 확인 한 후 판매자에게 돈을 지불한다.
-     * @param itemNo 상품 번호
-     * @return result 정상 지급 Y/N
-     * */
-    protected boolean sendMoneyCompanyToSeller(Long itemNo) {
-        boolean result = false;
-        logger.info("----------------------");
-        logger.info("sendMoneyCompanyToSeller method");
-        logger.debug("parameter itemNo : " + itemNo);
-        logger.info("----------------------");
-        return result;
-    }
 
-    /**
-     * 구매취소 후 구매자에게 돈을 환급하는 메소드로 <code>itemNo</code>인 상품의 금액을 찾아고 구매자의 정보를 찾은 후 구매자에게 돈을 환급한다
-     * <code>itemNo</code>가 <code>null</code>인 경우 NullPointException을 반환한다.
-     * @param itemNo 상품 번호
-     * @return result  정상 환급 Y/N
-     * */
-    protected boolean sendMoneyCompanyToBuyer(Long itemNo) {
-        boolean result = false;
-        logger.info("----------------------");
-        logger.info("sendMoneyCompanyToBuyer method");
-        logger.debug("parameter itemNo : " + itemNo);
-        logger.info("----------------------");
-        return result;
-    }
 }
