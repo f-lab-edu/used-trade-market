b97e985c8c66566173aa58b57d0e631372b024ba
YoungHyun Bae <byh2949@gmail.com>
2022-04-27 19:52:38 +0900

    spring security, oauth2 서버 설정 관련 부분 커밋

---
+++ app/src/main/java/com/market/config/SecurityConfig.java
@@ -0,0 +1,84 @@
+package com.market.config;
+
+import com.market.dto.fix.Role;
+import com.market.service.CustomOAuth2UserService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.security.web.access.expression.WebExpressionVoter;
+
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author 배영현
+ * Spring Security 관련 설정
+ * */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+        this.customOAuth2UserService = customOAuth2UserService;
+    }
+
+    public AccessDecisionManager accessDecisionManager() {
+        // admin도 user 권한에 접근할 수 있도록 hierarchy 설정
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+
+        // express handler
+        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+        handler.setRoleHierarchy(roleHierarchy);
+
+        // Voter 설정
+        WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
+
+        webExpressionVoter.setExpressionHandler(handler);
+        List<AccessDecisionVoter<? extends  Object>> voters = Arrays.asList(webExpressionVoter);
+
+        return new AffirmativeBased(voters);
+    }
+
+    /**
+     * 정적 리소스에는 적용 안되도록 설정
+     * */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .mvcMatchers("/member/**").hasRole(Role.ADMIN.name())
+                .anyRequest().authenticated().accessDecisionManager(accessDecisionManager())
+                .and()
+                .formLogin().loginPage("/login")
+                .and()
+                .oauth2Login().loginPage("/login")
+                .and()
+                .logout().logoutSuccessUrl("/")
+                .and()
+                .oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+
+    }
+}
